9f3a2761abadaba9d1ebd3dbd42710862948dc93
George Psimenos <drcref@gmail.com>
2017-11-16 13:44:38 +0000

    board/arcticus-rev2: added support for stm32l4-based arcticus board

---
+++ boards/arcticus-rev2/board.c
@@ -0,0 +1,8 @@
+#include "board.h"
+#include "periph/gpio.h"
+
+void board_init(void)
+{
+    /* initialize the CPU */
+    cpu_init();
+}
---
+++ boards/arcticus-rev2/include/board.h
@@ -0,0 +1,11 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+#include "board_common.h"
+
+#ifdef __cplusplus
+extern "C" {}
+#endif
+
+#endif /* BOARD_H */
+/** @} */
